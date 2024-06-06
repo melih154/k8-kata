173662acf336546f3561763c0d464d8da1a9804f
Okdft <sinem.okozturk@dft.gov.uk>
2024-06-07 00:25:13 +0100

    Add initial Docker implementation

---
+++ K8.Pricing/Program.cs
@@ -0,0 +1,15 @@
+﻿using StackExchange.Redis;
+
+using ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("172.17.0.2:6379");
+IDatabase db = redis.GetDatabase();
+
+var counter = 0;
+var max = args.Length is not 0 ? Convert.ToInt32(args[0]) : -1;
+Guid uniqueId = Guid.NewGuid();
+while (max is -1 || counter < max)
+{
+    Console.WriteLine($"Counter: {++counter}");
+    await Task.Delay(TimeSpan.FromMilliseconds(1_000));
+
+    db.StringSet($"mykey-{uniqueId}-{counter}", Guid.NewGuid().ToString());
+}
---
+++ K8.Pricing/README.md
@@ -0,0 +1,19 @@
+﻿This project is a simple example of how to use Redis with .NET using container technology.
+
+
+### Docker
+
+To run redis in a docker container, run the following command:
+`docker run --name redis -d -p 6379:6379 redis`
+
+To connect to the redis server, run the following command:
+`npm install -g redis-cli`
+
+To connect to the redis server, run the following command:
+`rdcli`
+
+To list all keys in the redis server, run the following command:
+`KEYS my*`
+
+### Kubernetes
+
